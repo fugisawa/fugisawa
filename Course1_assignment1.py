cabe9d28a571684d9b27718e5c8678c128ec0db5
fugisawa <76534741+fugisawa@users.noreply.github.com>
2024-04-14 00:44:44 -0300

    Initial commit

---
+++ Course1_assignment1.py
@@ -0,0 +1,6 @@
+# Variables
+name = input("What is your name? ")
+country = input("What country were you born in? ")
+age = input("How old are you? ")
+# Create a very simple story using the input from the user
+print(f"Once upon a time, there was a person named {name}. {name} was born in {country}. {name} was {age} years old, when {name} decided to take a Python course on Coursera. After completing the course, {name} became a Python programmer and lived happily ever after. The end.")
